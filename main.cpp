1ff7292bfa633960bf6733ec6765876d11449639
Gentilhomme <gentilhomme.thomas@gmail.com>
2017-05-04 16:31:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,31 +5,17 @@
 
 using namespace std;
 
-#ifdef __cplusplus
-extern "C"{
-#endif
-
-void nimInit(int);
-void nimEnd(int);
-void nimLogSet(string,string,int,int);
-void nimLogTruncateSize(int);
-void nimLog(int,string);
-
-#ifdef __cplusplus
-};
-#endif
-
 int main()
 {
-    nimInit(0);
+    nimInit (0);
 
-    nimLogSet("example_probe.log","example_probe", 0, 0);
-    nimLogTruncateSize(100 * 1024);
+    nimLogSet ("example_probe.log","example_probe", 0, 0);
+    nimLogTruncateSize (100 * 1024);
 
-    nimLog(0,"****************[ Starting ]****************");
-    nimLog(0,"Hello world!!!");
+    nimLog (0,"****************[ Starting ]****************");
+    nimLog (0,"Hello world!!!");
 
     cout << "Hello world!" << endl;
-    nimEnd(0);
+    nimEnd (0);
     return 0;
 }
